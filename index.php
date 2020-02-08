1abdda3a1c56b192a54697cc89a0c335be23a422
drai06 <mateo.llano1@gmail.com>
2020-02-08 04:54:22 -0500

    new parameter

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  <body>
  <?php 
 	if (isset($_GET['nombre'])) {
-		echo '<h1<¡Hola ' .$_GET['nombre'].'!</h1>';
+		echo '<h1>¡Hola ' .$_GET['nombre'].'!</h1>';
 	} else {
 		echo '<h1>¡Hola Mundo!</h1>' 
 	}
